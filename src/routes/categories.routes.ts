82fed6dc7759d6527dd2f7b239d00412a6e60fe3
leticia-de-araujo <leticiadearaujonunes@gmail.com>
2022-10-05 20:15:30 -0300

    feat: creation of controllers and services folders for categories and comments

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const routes = Router();
+
+const categoriesRoutes = () => {
+  return routes;
+};
+
+export default categoriesRoutes;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,14 @@
 import { Express } from "express";
 import articlesRoutes from "./articles.routes";
 import authorsRoutes from "./authors.routes";
+import categoriesRoutes from "./categories.routes";
 import loginRoute from "./login.routes";
 
 const appRoutes = (app: Express) => {
   app.use("/authors", authorsRoutes());
   app.use("/login", loginRoute());
   app.use("/articles", articlesRoutes());
+  app.use("/categories", categoriesRoutes());
 };
 
 export default appRoutes;

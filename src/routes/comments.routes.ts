a79c1c3c52623e8cdf0379ad6143e003a45a96d8
leticia-de-araujo <leticiadearaujonunes@gmail.com>
2022-10-06 01:07:59 -0300

    feat: update and delete comment routes

--- src/routes/comments.routes.ts
+++ src/routes/comments.routes.ts
@@ -1,11 +1,17 @@
 import { Router } from "express";
 import commentCreateController from "../controllers/comments/commentCreate.controller";
+import commentDeleteController from "../controllers/comments/commentDelete.controller";
 import commentListAllController from "../controllers/comments/commentListAll.controller";
 import commentListOneController from "../controllers/comments/commentListOne.controller";
+import commentUpdateController from "../controllers/comments/commentUpdate.controller";
 import {
   commentCreateMiddleware,
   commentCreateSchema,
 } from "../middlewares/schemas/comments/commentCreateSchema.middleware";
+import {
+  commentUpdateMiddleware,
+  commentUpdateSchema,
+} from "../middlewares/schemas/comments/commentUpdateSchema.middleware";
 
 const routes = Router();
 
@@ -17,6 +23,12 @@ const commentsRoutes = () => {
   );
   routes.get("", commentListAllController);
   routes.get("/:id", commentListOneController);
+  routes.patch(
+    "/:id",
+    commentUpdateMiddleware(commentUpdateSchema),
+    commentUpdateController
+  );
+  routes.delete("/:id", commentDeleteController);
 
   return routes;
 };
--- src/services/comments/commentDelete.service.ts
+++ src/services/comments/commentDelete.service.ts
@@ -1,3 +1,21 @@
-const commentDeleteService = async () => {};
+import AppDataSource from "../../data-source";
+import { Comment } from "../../entities/comments.entity";
+import AppError from "../../errors/AppError";
+
+const commentDeleteService = async (id: string) => {
+  const commentsRepository = AppDataSource.getRepository(Comment);
+
+  const comments = await commentsRepository.find();
+
+  const comment = comments.find((comment) => comment.id === id);
+
+  if (!comment) {
+    throw new AppError(404, "Comment not found.");
+  }
+
+  await commentsRepository.delete(comment.id);
+
+  return true;
+};
 
 export default commentDeleteService;

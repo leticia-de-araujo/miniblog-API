d1da457ade684f5f3a431b31c4457b6cadfd169e
leticia-de-araujo <leticiadearaujonunes@gmail.com>
2022-10-05 22:33:15 -0300

    feat: delete category route

--- src/controllers/categories/categoryDelete.controller.ts
+++ src/controllers/categories/categoryDelete.controller.ts
@@ -1,3 +1,20 @@
-const categoryDeleteController = () => {};
+import { Response, Request } from "express";
+import AppError from "../../errors/AppError";
+import categoryDeleteService from "../../services/categories/categoryDelete.service";
+const categoryDeleteController = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    await categoryDeleteService(id);
+
+    return res.status(200).json({
+      message: "Category deleted successfully.",
+    });
+  } catch (error) {
+    if (error instanceof AppError) {
+      throw new AppError(error.statusCode, error.message);
+    }
+  }
+};
 
 export default categoryDeleteController;
--- src/services/categories/categoryDelete.service.ts
+++ src/services/categories/categoryDelete.service.ts
@@ -1,3 +1,21 @@
-const categoryDeleteService = () => {};
+import AppDataSource from "../../data-source";
+import { Category } from "../../entities/categories.entity";
+import AppError from "../../errors/AppError";
+
+const categoryDeleteService = async (id: string) => {
+  const categoriesRepository = AppDataSource.getRepository(Category);
+
+  const categories = await categoriesRepository.find();
+
+  const category = categories.find((category) => category.id === id);
+
+  if (!category) {
+    throw new AppError(404, "Category not found.");
+  }
+
+  await categoriesRepository.delete(category.id);
+
+  return true;
+};
 
 export default categoryDeleteService;
